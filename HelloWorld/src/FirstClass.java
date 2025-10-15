d75accbbcb91bf17fbb8588660233c2a6b1372b7
AlexP <pais.alex1991@gmail.com>
2025-10-15 18:47:36 +0300

    Create Hello class

--- HelloWorld/src/FirstClass.java
+++ HelloWorld/src/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.print("Hello and replace word with just another thing ");
 
     }
 }
--- HelloWorld/src/SecondClass.java
+++ HelloWorld/src/SecondClass.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class SecondClass {
+    public static void main(String[] args) {
+        System.out.print("Hello and replace word with just another thing ");
+
+    }
 }
