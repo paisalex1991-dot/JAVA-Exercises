d1d771e5af56fd161385f586e1408dd36558e6e1
AlexP <pais.alex1991@gmail.com>
2025-10-16 10:58:42 +0300

    Finished IfKeywordsAndCodeBlocks section

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -1,6 +1,35 @@
-public class Main {
-    public static void main (String[] args){
+public class SpeedConverter {
+    public static int toMilesPerHour (double KilometersPerHour){
 
+        long status;
+
+        if(KilometersPerHour < 0)
+        {
+            status = -1;
+        }
+        else
+        {
+            status = Math.round(KilometersPerHour * 0.621371192);
+        }
+
+        return (int) status;
     }
 
+
+    public static void printConversion (double kilometersPerHour){
+
+        long milesPerHour;
+
+        milesPerHour = Math.round(kilometersPerHour * 0.621371192);
+
+        if (kilometersPerHour >= 0)
+        {
+            System.out.print (kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+        else
+        {
+            System.out.print ("Invalid Value");
+        }
+    }
 }
+
