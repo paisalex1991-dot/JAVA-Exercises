512e74f800211281a3c6c8c8e65603a0d8ab0544
AlexP <pais.alex1991@gmail.com>
2025-10-17 13:06:01 +0300

    Create switch project, add Main.java calss

---
+++ Switch/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        int value = 3;
+
+        switch (value)
+        {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            default:
+                System.out.println("Value was not 1 or 2");
+
+
+        }
+    }
+}
