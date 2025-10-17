b8b31a6c31304357714e15c35856c451b146712a
AlexP <pais.alex1991@gmail.com>
2025-10-17 13:18:50 +0300

    Example of Enhanced Switch Statement

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -6,21 +6,13 @@ public class Main {
 
         switch (value)
         {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-
-            case 2:
-                System.out.println("Value was 2");
-                break;
-
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Value was a 3 a 4 or a 5");
                 System.out.println("Acrual case was: " + value);
-                break;
-
-            default:
-                System.out.println("Value was not 1 or 2");
+            }
+            default -> System.out.println("Value was not 1 or 2");
 
 
         }
