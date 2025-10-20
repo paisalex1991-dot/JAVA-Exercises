47733f8fab13be35e7f747c27e47e494ba20c26c
AlexP <pais.alex1991@gmail.com>
2025-10-20 21:31:53 +0300

    Modify Main class

--- ThePrimeNumberChallenge/src/Main.java
+++ ThePrimeNumberChallenge/src/Main.java
@@ -2,10 +2,19 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("0 is " + ((isPrime(0)) ? "" :  " NOT") + " a prime number");
-        System.out.println("1 is " + ((isPrime(1)) ? "" :  " NOT") + " a prime number");
-        System.out.println("2 is " + ((isPrime(2)) ? "" :  " NOT") + " a prime number");
-        System.out.println("3 is " + ((isPrime(3)) ? "" :  " NOT") + " a prime number");
+      int primeCounter = 0;
+
+      for (int i=0; i<=1000; i++){
+
+          if(isPrime(i)){
+              primeCounter++;
+              System.out.println(i + " is a prime number!");
+          }
+
+          if (primeCounter == 3){
+              break;
+          }
+      }
 
     }
 
