7249f27528fe3169aaf5e1eae6b58cfed31cce13
AlexP <pais.alex1991@gmail.com>
2025-10-24 16:27:35 +0300

    Add exercise 34

---
+++ Exercise34-Udemy-Floor/src/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        double cost;
+
+        return cost = floor.getArea() * carpet.getCost();
+    }
+}
