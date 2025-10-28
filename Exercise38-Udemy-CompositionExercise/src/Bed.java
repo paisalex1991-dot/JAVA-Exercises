8af6399fa25491970415451b85301f69c0cb6e00
AlexP <pais.alex1991@gmail.com>
2025-10-28 20:28:52 +0200

    Add Exercise 38

---
+++ Exercise38-Udemy-CompositionExercise/src/Bed.java
@@ -0,0 +1,37 @@
+public class Bed {
+
+    private String style;
+    private int pillows;
+    private int height;
+    private int quilt;
+    private int sheets;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt){
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilt = quilt;
+    }
+
+    public void make(){
+
+        System.out.print("Bed -> Making | ");
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getQuilt() {
+        return quilt;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+}
