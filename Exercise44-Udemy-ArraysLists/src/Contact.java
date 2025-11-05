d35c3725e98e4bdb05a205eba1d66f682b69b835
AlexP <pais.alex1991@gmail.com>
2025-11-05 21:49:29 +0200

    Add exercise 44

---
+++ Exercise44-Udemy-ArraysLists/src/Contact.java
@@ -0,0 +1,27 @@
+public class Contact {
+
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public static Contact createContact(String name, String phoneNumber){
+
+       Contact contact = new Contact(name, phoneNumber);
+       return contact;
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
