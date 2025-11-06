45da04835995abd2d1d6ebc517af8036da77024d
AlexP <pais.alex1991@gmail.com>
2025-11-06 18:07:40 +0200

    Add retreiving/removing methods to linked lists

--- LinkedLists/src/dev/lpa/Main.java
+++ LinkedLists/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -16,8 +17,13 @@ public class Main {
         addMoreElements(placesToVisit);
         System.out.println(placesToVisit);
 
-        removeElements(placesToVisit);
-        System.out.println(placesToVisit);
+//        removeElements(placesToVisit);
+//        System.out.println(placesToVisit);
+
+ //       getElements(placesToVisit);
+//        System.out.println(placesToVisit);
+
+        printItinerary2(placesToVisit);
 
 
 
@@ -42,6 +48,70 @@ public class Main {
         list.remove("brisbane"); // removes brisabane from the list
 
         String s1 = list.remove(); // removes the first element from the list and add it to s1
+
+
+    }
+
+    private static void getElements(LinkedList<String> list){
+
+        System.out.println("Retrived element = " + list.get(4)); // get the fifth element from the list
+
+        System.out.println("First element = " + list.getFirst()); // get the first element from the list
+        System.out.println("Last element = " + list.getLast()); // get the last element from the list
+
+        System.out.println("Darwin is at position: " + list.indexOf("Darwin")); // gets the position where Darwin is
+        System.out.println("melborne is at position " + list.lastIndexOf("melborne")); //get the position where melborne is
+
+        //Queue retrieval methods
+        System.out.println("Element from element() = " + list.element()); // return first element from list
+
+        //Stack retreival methods
+        System.out.println("Element form peak() method = " + list.peek()); //gets first element from the list
+        System.out.println("Element form peakFirst() method = " + list.peekFirst()); //gets first element from the list
+        System.out.println("Element form peakLast() method = " + list.peekLast()); // gets last element from the list
+    }
+
+    public static void printItinerary(LinkedList<String> list){
+
+        System.out.println("Trip starts at " + list.getFirst());
+
+        for (int i = 1; i < list.size(); i++){
+
+            System.out.println("--> From: " + list.get(i-1) + " to " + list.get(i));
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
+
+
+    }
+
+    public static void printItinerary2(LinkedList<String> list){
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        for (String town : list){
+            System.out.println("--> From: " + previousTown + " to "+ town);
+            previousTown = town;
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
+
+
+    }
+
+    public static void printItinerary3(LinkedList<String> list){
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator();
+
+        while(iterator.hasNext()){
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to "+ town);
+            previousTown = town;
+        }
+
+        System.out.println("Trip ends at " + list.getLast());
 
 
     }
