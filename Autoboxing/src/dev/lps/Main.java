4dd34477534b736ba576639a75f2e74b6629b13d
AlexP <pais.alex1991@gmail.com>
2025-11-12 15:04:31 +0200

    Update

--- Autoboxing/src/dev/lps/Main.java
+++ Autoboxing/src/dev/lps/Main.java
@@ -1,4 +1,29 @@
 package dev.lps;
 
 public class Main {
+
+    public static void main (String[] args){
+
+        Integer boxedInt = Integer.valueOf(15);
+       // Integer deprecatedBoxing = new Integer(15); //old code nok
+        int unboxedInt = boxedInt.intValue();
+
+        //Automatic boxing
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+        //System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+
+        }
+
+        private static Double getDoubleObject(){
+        return Double.valueOf(100.00);
+        }
+
+        private static Double getLiteralDoublePrimitive() {
+            return 100.00;
+        }
 }
