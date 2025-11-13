d897a4af8aab81d1c94b6b247623c53475719a0d
AlexP <pais.alex1991@gmail.com>
2025-11-13 19:36:06 +0200

    Abstract challenge update

--- AbstractClasses-Challenge/src/Store.java
+++ AbstractClasses-Challenge/src/Store.java
@@ -21,6 +21,31 @@ public class Store {
             System.out.println("-".repeat(30));
             item.showDetails();
 
+            System.out.println("\nOrder 1");
+            var order1 = new ArrayList<OrderItem>();
+            addItemToOrder(order1, 1, 5);
+            addItemToOrder(order1, 0, 2);
+            printOrder(order1);
+
+
+
+
         }
+    }
+
+    public static void addItemToOrder(ArrayList<OrderItem> order, int orderIndex, int qty){
+
+        order.add(new OrderItem(qty, storeProducts.get(orderIndex)));
+    }
+
+    public static void printOrder(ArrayList<OrderItem> order){
+        double salesTotal = 0;
+        for(var item : order){
+            item.product().printPricedItem(item.qty());
+            salesTotal += item.product().getSalesPrice(item.qty());
+
+        }
+        System.out.printf("Sales total = $%6.2f %n", salesTotal);
+
     }
 }
